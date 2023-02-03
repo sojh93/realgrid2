4305e9633503e1e7c19d40c714390b90b9c9f32c
JeongHyeonSo <36619557+sojh93@users.noreply.github.com>
2023-02-03 13:50:13 +0900

    컨텍스트 메뉴 설정완료
    컨텍스트 메뉴 설정완료

--- index.js
+++ index.js
@@ -1,238 +1,255 @@
 // 필드 생성
 
 var fields = [
-    {
-      fieldName: "KorName",
-      dataType: "text"
+  {
+    fieldName: "KorName",
+    dataType: "text"
+  },
+  {
+    fieldName: "Gender",
+    dataType: "text"
+  },
+  {
+    fieldName: "Age",
+    dataType: "number"
+  },
+  {
+    fieldName: "Phone",
+    dataType: "text"
+  },
+  {
+    fieldName: "ProductId",
+    dataType: "text"
+  },
+  {
+    fieldName: "KorCountry",
+    dataType: "text"
+  },
+  {
+    fieldName: "OrderDate",
+    dataType: "datetime",
+    datetimeFormat: "yyyy-MM-dd",
+    amText: "오전",
+    pmText: "오후"
+  },
+  {
+    fieldName: "CardNumber",
+    dataType: "text"
+  },
+  {
+    fieldName: "Monetary",
+    dataType: "text"
+  },
+  {
+    fieldName: "StartDate",
+    dataType: "datetime",
+    datetimeFormat: "yyyy-MM-dd",
+    amText: "오전",
+    pmText: "오후"
+  },
+  {
+    fieldName: "EndDate",
+    dataType: "datetime",
+    datetimeFormat: "yyyy-MM-dd",
+    amText: "오전",
+    pmText: "오후"
+  },
+  {
+    fieldName: "ToMonth",
+    dataType: "number"
+  },
+  {
+    fieldName: "Month",
+    dataType: "number"
+  },
+  {
+    fieldName: "Year",
+    dataType: "number"
+  },
+  {
+    fieldName: "InterestRate",
+    dataType: "number"
+  },
+  {
+    fieldName: "SaveCost",
+    dataType: "number"
+  },
+  {
+    fieldName: "SaveMaturity",
+    dataType: "number"
+  },
+  {
+    fieldName: "CurrentSave",
+    dataType: "number"
+  }
+];
+
+var columns = [
+  {
+    name: "KorName",
+    fieldName: "KorName",
+    width: "60",
+    header: {
+      text: "이름"
     },
-    {
-      fieldName: "Gender",
-      dataType: "text"
-    },
-    {
-      fieldName: "Age",
-      dataType: "number"
-    },
-    {
-      fieldName: "Phone",
-      dataType: "text"
-    },
-    {
-      fieldName: "ProductId",
-      dataType: "text"
-    },
-    {
-      fieldName: "KorCountry",
-      dataType: "text"
-    },
-    {
-      fieldName: "OrderDate",
-      dataType: "datetime",
-      datetimeFormat: "yyyy-MM-dd",
-      amText: "오전",
-      pmText: "오후"
-    },
-    {
-      fieldName: "CardNumber",
-      dataType: "text"
-    },
-    {
-      fieldName: "Monetary",
-      dataType: "text"
-    },
-    {
-      fieldName: "StartDate",
-      dataType: "datetime",
-      datetimeFormat: "yyyy-MM-dd",
-      amText: "오전",
-      pmText: "오후"
-    },
-    {
-      fieldName: "EndDate",
-      dataType: "datetime",
-      datetimeFormat: "yyyy-MM-dd",
-      amText: "오전",
-      pmText: "오후"
-    },
-    {
-      fieldName: "ToMonth",
-      dataType: "number"
-    },
-    {
-      fieldName: "Month",
-      dataType: "number"
-    },
-    {
-      fieldName: "Year",
-      dataType: "number"
-    },
-    {
-      fieldName: "InterestRate",
-      dataType: "number"
-    },
-    {
-      fieldName: "SaveCost",
-      dataType: "number"
-    },
-    {
-      fieldName: "SaveMaturity",
-      dataType: "number"
-    },
-    {
-      fieldName: "CurrentSave",
-      dataType: "number"
+    renderer: {
+      type: "text"
     }
-  ];
-  
-  var columns = [
-    {
-      name: "KorName",
-      fieldName: "KorName",
-      width: "60",
-      header: {
-        text: "이름"
-      }
+  },
+  {
+    name: "Gender",
+    fieldName: "Gender",
+    width: "40",
+    header: {
+      text: "성별"
     },
-    {
-      name: "Gender",
-      fieldName: "Gender",
-      width: "40",
-      header: {
-        text: "성별"
-      }
-    },
-    {
-      name: "Age",
-      fieldName: "Age",
-      width: "40",
-      header: {
-        text: "나이"
-      },
-      styleName: "right-column"
-    },
-    {
-      name: "Phone",
-      fieldName: "Phone",
-      width: "100",
-      header: {
-        text: "전화번호"
-      }
-    },
-    {
-      name: "ProductId",
-      fieldName: "ProductId",
-      width: "120",
-      header: {
-        text: "제품번호"
-      }
-    },
-    {
-      name: "KorCountry",
-      fieldName: "KorCountry",
-      width: "100",
-      header: {
-        text: "투자국가"
-      }
-    },
-    {
-      name: "OrderDate",
-      fieldName: "OrderDate",
-      width: "100",
-      header: {
-        text: "주문일자"
-      }
-    },
-    {
-      name: "CardNumber",
-      fieldName: "CardNumber",
-      width: "140",
-      header: {
-        text: "카드번호"
-      }
-    },
-    {
-      name: "Monetary",
-      fieldName: "Monetary",
-      width: "40",
-      header: {
-        text: "통화"
-      }
-    },
-    {
-      name: "StartDate",
-      fieldName: "StartDate",
-      width: "100",
-      header: {
-        text: "최초납입일"
-      }
-    },
-    {
-      name: "EndDate",
-      fieldName: "EndDate",
-      width: "100",
-      header: {
-        text: "종료일"
-      }
-    },
-    {
-      name: "ToMonth",
-      fieldName: "ToMonth",
-      width: "50",
-      header: {
-        text: "납입 횟수"
-      },
-      styleName: "right-column"
-    },
-    {
-      name: "Month",
-      fieldName: "Month",
-      width: "50",
-      header: {
-        text: "남은 횟수"
-      },
-      styleName: "right-column"
-    },
-    {
-      name: "InterestRate",
-      fieldName: "InterestRate",
-      width: "50",
-      numberFormat: "0.00",
-      header: {
-        text: "이율"
-      },
-      styleName: "right-column"
-    },
-    {
-      name: "SaveCost",
-      fieldName: "SaveCost",
-      width: "80",
-      numberFormat: "#,##0",
-      header: {
-        text: "납입금"
-      },
-      styleName: "right-column"
-    },
-    {
-      name: "SaveMaturity",
-      fieldName: "SaveMaturity",
-      width: "80",
-      numberFormat: "#,##0",
-      header: {
-        text: "만기금액"
-      },
-      styleName: "right-column"
-    },
-    {
-      name: "CurrentSave",
-      fieldName: "CurrentSave",
-      width: "80",
-      numberFormat: "#,##0",
-      header: {
-        text: "현재잔액"
-      },
-      styleName: "right-column"
+    renderer: {
+      type: "text"
     }
-  ];
+  },
+  {
+    name: "Age",
+    fieldName: "Age",
+    width: "40",
+    header: {
+      text: "나이"
+    },
+    renderer: {
+      type: "text"
+    }
+  },
+  {
+    name: "Phone",
+    fieldName: "Phone",
+    width: "100",
+    header: {
+      text: "전화번호"
+    },
+    renderer: {
+      type: "text"
+    }
+  },
+  {
+    name: "ProductId",
+    fieldName: "ProductId",
+    width: "120",
+    header: {
+      text: "제품번호"
+    },
+    renderer: {
+      type: "text"
+    }
+  },
+  {
+    name: "KorCountry",
+    fieldName: "KorCountry",
+    width: "100",
+    header: {
+      text: "투자국가"
+    },
+    renderer: {
+      type: "text"
+    }
+  },
+  {
+    name: "OrderDate",
+    fieldName: "OrderDate",
+    width: "100",
+    header: {
+      text: "주문일자"
+    }
+  },
+  {
+    name: "CardNumber",
+    fieldName: "CardNumber",
+    width: "140",
+    header: {
+      text: "카드번호"
+    },
+    renderer: {
+      type: "text"
+    }
+  },
+  {
+    name: "Monetary",
+    fieldName: "Monetary",
+    width: "40",
+    header: {
+      text: "통화"
+    },
+    renderer: {
+      type: "text"
+    }
+  },
+  {
+    name: "StartDate",
+    fieldName: "StartDate",
+    width: "100",
+    header: {
+      text: "최초납입일"
+    }
+  },
+  {
+    name: "EndDate",
+    fieldName: "EndDate",
+    width: "100",
+    header: {
+      text: "종료일"
+    }
+  },
+  {
+    name: "ToMonth",
+    fieldName: "ToMonth",
+    width: "40",
+    header: {
+      text: "납입 횟수"
+    }
+  },
+  {
+    name: "Month",
+    fieldName: "Month",
+    width: "40",
+    header: {
+      text: "남은 횟수"
+    }
+  },
+  {
+    name: "InterestRate",
+    fieldName: "InterestRate",
+    width: "40",
+    numberFormat: "0.00",
+    header: {
+      text: "이율"
+    }
+  },
+  {
+    name: "SaveCost",
+    fieldName: "SaveCost",
+    width: "80",
+    numberFormat: "#,##0",
+    header: {
+      text: "납입금"
+    }
+  },
+  {
+    name: "SaveMaturity",
+    fieldName: "SaveMaturity",
+    width: "80",
+    numberFormat: "#,##0",
+    header: {
+      text: "만기금액"
+    }
+  },
+  {
+    name: "CurrentSave",
+    fieldName: "CurrentSave",
+    width: "80",
+    numberFormat: "#,##0",
+    header: {
+      text: "현재잔액"
+    }
+  }
+];
   
   var httpRequest;
   
@@ -248,6 +265,7 @@ var fields = [
       if (httpRequest.status === 200) {
         var data = httpRequest.responseText;
         dataProvider.fillXmlData(data);
+        dataProvider.setRows(data);
         gridView.refresh();
       }
     }
@@ -274,9 +292,11 @@ var fields = [
     gridView.editOptions.insertable = true;
     gridView.editOptions.appendable = true;
   
-  
+    // 체크바 & 데이터 필드 연동
+    gridView.checkBar.fieldName = "Bool2"; 
   
     fillXmlData();
+    setContextMenu(gridView);
   }
   
   function start() {
@@ -296,14 +316,31 @@ var fields = [
     gridView = null;
     dataProvider = null;
   }
+
+/*   function btnSetContextMenu() {
+    gridView.setContextMenu([
+      {
+        label: "Menu1"
+      },
+      {
+        label: "Menu2"
+      },
+      {
+        label: "-" // menu separator를 삽입합니다.
+      },
+      {
+        label: "ExcelExport"
+      }
+    ]);
+  } */
   
-  // 실제 들어가는 데이터
+  // 실제 들어가는 데이터(초기값)
   function fillXmlData() {
     var data = '<rows>    <row KorName= "박영호" Gender= "남" Age= "71" Phone= "(025)6563-2802" ProductId= "198160731-00008" KorCountry= "모잠비크" OrderDate= "2021-01-16" CardNumber= "5587-2139-9692-3644" Monetary= "EUR" StartDate= "2018-02-25" EndDate= "2021-08-12" ToMonth= "23" Month= "41" Year= "3" InterestRate= "0.15" SaveCost= "51000" SaveMaturity= "14950650" CurrentSave= "9304950"  Rating= "5" BusinessProficiency= "59" Address= "서울특별시 강서구 공항동 45-89"/>    <row KorName= "조일형" Gender= "남" Age= "62" Phone= "(093)8809-8696" ProductId= "571215854-00001" KorCountry= "캐나다" OrderDate= "2019-07-29" CardNumber= "5348-5093-3750-0623" Monetary= "USD" StartDate= "2019-10-21" EndDate= "2022-12-11" ToMonth= "3" Month= "37" Year= "3" InterestRate= "0.38" SaveCost= "14000" SaveMaturity= "7801080" CurrentSave= "1108520" Rating= "3" BusinessProficiency= "53" Address= "서울특별시 중구 봉래동2가 122" />  </rows>';
   
     dataProvider.fillXmlData(data, { fillMode: "set" });
   }
-//   마지막 줄에 추가
+//   마지막 줄에 추가(append)
   function appendData() {
     var data = '<rows>\
       <row KorName= "박영호" Gender= "남" Age= "71" Phone= "(025)6563-2802" ProductId= "198160731-00008" KorCountry= "모잠비크" OrderDate= "2021-01-16" CardNumber= "5587-2139-9692-3644" Monetary= "EUR" StartDate= "2018-02-25" EndDate= "2021-08-12" ToMonth= "23" Month= "41" Year= "3" InterestRate= "0.15" SaveCost= "51000" SaveMaturity= "14950650" CurrentSave= "9304950"  Rating= "5" BusinessProficiency= "59" Address= "서울특별시 강서구 공항동 45-89"/>\
@@ -312,7 +349,7 @@ var fields = [
   
     dataProvider.fillXmlData(data, { fillMode: "append", start: 0, count: 1 });
   }
-  // 클릭 열 위로 추가됨
+  // 클릭 열 위로 추가됨(insert)
   function insertData() {
     var data = '<rows>\
       <row KorName= "소정현" Gender= "남" Age= "31" Phone= "(025)6563-2802" ProductId= "198160731-00008" KorCountry= "모잠비크" OrderDate= "2021-01-16" CardNumber= "5587-2139-9692-3644" Monetary= "EUR" StartDate= "2018-02-25" EndDate= "2021-08-12" ToMonth= "23" Month= "41" Year= "3" InterestRate= "0.15" SaveCost= "51000" SaveMaturity= "14950650" CurrentSave= "9304950"  Rating= "5" BusinessProficiency= "59" Address= "서울특별시 강서구 공항동 45-89"/>\
@@ -360,3 +397,156 @@ var fields = [
       mark: "image"
     });
   }
+
+  function btnOnContextMenuPopup() {
+    gridView.onContextMenuPopup = function(grid, x, y, elementName) {
+      //헤더셀 영역에서는 컨텍스트 메뉴 실행하지 않음
+      return elementName.cellType != "header";
+    };
+  }
+
+  var toggle = false;
+function setContextMenu(grid) {
+   grid.onContextMenuItemClicked = function (grid, item, clickData) { 
+       if (item.tag == "excel") {
+           grid.exportGrid({
+               type: "excel",
+               target: "local",
+               fileName: "gridExportSample.xlsx"
+           });
+       } else if (item.tag == 'filter' && clickData.column) {
+           createColumnFilter(grid, clickData.column);
+       } else if (item.tag == 'visibleTrue') {
+           var columns = grid.getColumns();
+
+           for (var i in columns) {
+               grid.setColumnProperty(columns[i].name, "visible", true);
+           }
+           toggle = false;
+           setHeaderCellContextMenu(grid, toggle);
+       } else if (item.tag == 'visibleFalse') {
+           grid.setColumnProperty(clickData.column, "visible", false);
+
+           toggle = true;
+           setHeaderCellContextMenu(grid, toggle);
+       } else if (item.tag == 'fixedCol') {
+           var count = grid.getColumnProperty(clickData.column, "displayIndex") + 1;
+           grid.setFixedOptions({ colCount: count });
+       } else if (item.tag == 'fixedRow') {
+           var count = clickData.itemIndex + 1;
+           grid.setFixedOptions({ rowCount: count });
+       } else if (item.tag == 'fixedCancel') {
+           grid.setFixedOptions({ colCount: 0, rowCount: 0 });
+       };
+   }
+
+   grid.onContextMenuPopup = function (grid, x, y, elementName) {
+       if (elementName.cellType == 'header') {
+           setHeaderCellContextMenu(grid, toggle);
+       } else if (elementName.cellType == 'data') {
+           setDataCellContextMenu(grid);
+       } else {
+           return false;
+       }
+   };
+
+   setDataCellContextMenu(grid);
+}
+
+function setHeaderCellContextMenu(grid, val) {
+   var contextMenu = [{
+       label: '엑셀 내보내기',
+       tag: 'excel'
+   }, {
+       label: '필터 만들기',
+       tag: 'filter'
+   }, {
+       label: "-"
+   }, {
+       label: '컬럼 숨기기',
+       tag: 'visibleFalse'
+   }, {
+       label: '컬럼 모두 보이기',
+       tag: 'visibleTrue',
+       enabled: val
+   }];
+
+   grid.setContextMenu(contextMenu);
+}
+
+function setDataCellContextMenu(grid) {
+   var contextMenu = [{
+       label: '엑셀 내보내기',
+       tag: 'excel'
+   }, {
+       label: "-"
+   }, {
+       label: '열 고정',
+       tag: 'fixedCol'
+   }, {
+       label: '행 고정',
+       tag: 'fixedRow'
+   }, {
+       label: '고정 취소',
+       tag: 'fixedCancel'
+   }];
+
+   grid.setContextMenu(contextMenu);
+}
+
+// 헤더 컨텍스트 메뉴
+function setHeaderCellContextMenu(grid, val) {
+   var contextMenu = [{
+       label: '엑셀 내보내기',
+       tag: 'excel'
+   }, {
+       label: '필터 만들기',
+       tag: 'filter'
+   }, {
+       label: "-"
+   }, {
+       label: '컬럼 숨기기',
+       tag: 'visibleFalse'
+   }, {
+       label: '컬럼 모두 보이기',
+       tag: 'visibleTrue',
+       enabled: val
+   }];
+
+   grid.setContextMenu(contextMenu);
+}
+
+// 컨텍스트 한글메뉴
+function setDataCellContextMenu(grid) {
+   var contextMenu = [{
+       label: '엑셀 내보내기',
+       tag: 'excel'
+   }, {
+       label: "-"
+   }, {
+       label: '열 고정',
+       tag: 'fixedCol'
+   }, {
+       label: '행 고정',
+       tag: 'fixedRow'
+   }, {
+       label: '고정 취소',
+       tag: 'fixedCancel'
+   }];
+
+   grid.setContextMenu(contextMenu);
+}
+
+function createColumnFilter(grid, colName) {
+    var fieldName = grid.getColumnProperty(colName, "fieldName");
+    var distinctValues = dataProvider.getDistinctValues(fieldName);
+    var filters = [];
+
+    for (var i = 0; i < distinctValues.length; i++) {
+        filters.push({ name: distinctValues[i], criteria: "value = " + "'" + distinctValues[i] + "'" });
+    }
+
+    grid.setColumnFilters(colName, filters);
+}
+
+setContextMenu(grid);
